2f99d4927a9d66a2e7999bc00d88d70ccc59e326
bryan alzate <bryanalzate89@gamail.com>
2024-04-22 19:14:22 -0500

    tareaalgoritmia

---
+++ BRYAN_ALZATE_1061718997/bryan-alzate-tarea-4-clase-5/tarea2numeroprimo.js
@@ -0,0 +1,30 @@
+function numeroprimo(numero) {
+    if(numero <= 0){
+        return false
+    }
+    let contador = 0;
+    for (var i = 1; i <= numero ; i++){
+        if (numero % i === 0)  {
+            contador ++ ;    
+        }     
+    }
+    return contador === 2 ? true : false ;
+}
+console.log ("el numero 1 es :" , numeroprimo (1));
+console.log ("el numero 2 es :" , numeroprimo (2));
+console.log ("el numero 3 es :" , numeroprimo (3));
+console.log ("el numero 4 es :" , numeroprimo (4));
+console.log ("el numero 5 es :" , numeroprimo (5));
+console.log ("el numero 6 es :" , numeroprimo (6));
+console.log ("el numero 7 es :" , numeroprimo (7));
+console.log ("el numero 8 es :" , numeroprimo (8));
+console.log ("el numero 9 es :" , numeroprimo (9));
+console.log ("el numero 10 es :" , numeroprimo (10));
+
+
+
+
+
+
+
+
