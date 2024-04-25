7b6eb94979b5c2a476502bb16630e2020e0deeb5
wilsonjs <wilson.bedoyajs@gmail.com>
2024-04-24 21:13:46 -0500

    Desarrollo de la tarea 5 clase 7 Wilson Bedoya

---
+++ WILSON_BEDOYA_9774453/Tarea-5-Clase -7/main.js
@@ -0,0 +1,64 @@
+console.log("-------------EJERCICIO 1-----------------------");
+let numeros = [1, 2, 3, 4, 5];
+let dobles=[];
+numeros.forEach((element)=> {
+    element=element*2;
+    dobles.push(element);
+});
+console.log(dobles);
+/*-------------ejercicio 2----------------------- */
+console.log("-------------EJERCICIO 2-----------------------");
+let productos = [
+    { nombre: "Camisa", precio: 20 },
+    { nombre: "Pantalón", precio: 30 },
+    { nombre: "Zapatos", precio: 50 },
+];
+let productoBarato = productos[0];//paso la posicion cero del array productos al nuevo array;
+productos.forEach((producto)=>{
+    if(productos.precio<productoBarato.precio){
+        productoBarato=producto;
+    }
+})
+console.log("El producto más barato es:", productoBarato.nombre);
+console.log("Precio:", productoBarato.precio);
+/*-------------ejercicio 3----------------------- */
+console.log("-------------EJERCICIO 3-----------------------");
+let estudiantes = [
+    { nombre: "Juan", edad: 20 },
+    { nombre: "María", edad: 22 },
+    { nombre: "Pedro", edad: 19 },
+];
+estudiantes.forEach((estudiante)=>{
+    estudiante.edad++;
+})
+console.log(estudiantes);
+/*-------------ejercicio 4----------------------- */
+console.log("-------------EJERCICIO 4-----------------------");
+let libros = [
+    { titulo: "El principito", autor: "Antoine de Saint-Exupéry" },
+    { titulo: "Don Quijote de la Mancha", autor: "Miguel de Cervantes" },
+    { titulo: "Cien años de soledad", autor: "Gabriel García Márquez" },
+  ];
+  let nuevoArrayLibros=[];
+  libros.forEach((libro)=>{
+    if (libro.titulo.startsWith('El')) {
+        nuevoArrayLibros.push(libro);        
+    }    
+  })
+  console.log(nuevoArrayLibros);
+  /*-------------ejercicio 5----------------------- */
+  console.log("-------------EJERCICIO 5-----------------------");
+  let frutas1 = ["Manzana", "Plátano", "Naranja"];
+  let frutas2 = ["Fresa", "Uva", "Piña"];
+  let totalFrutas=[];
+  /*Primera opcion usando bucle forEach */
+  frutas1.forEach((fruta)=>{
+    totalFrutas.push(fruta);
+  })
+  frutas2.forEach((fruta)=>{
+    totalFrutas.push(fruta);
+  })
+  console.log(totalFrutas);
+/*Segunda opcion usando el método concat() */
+  let totalFrutas2 = frutas1.concat(frutas2);
+  console.log(totalFrutas2);
