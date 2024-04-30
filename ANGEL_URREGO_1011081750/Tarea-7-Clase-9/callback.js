560ddfd31ed7dd6a94bef657046e3b1527754402
AngelUrrego <angelurrego2010@gmail.com>
2024-04-30 00:26:05 -0500

    Tarea-7-Clase-9

---
+++ ANGEL_URREGO_1011081750/Tarea-7-Clase-9/callback.js
@@ -0,0 +1,68 @@
+//EJERCICIO #1
+function sumarArreglo(numeros, callback) {
+    let sum = 0
+    numeros.forEach(element => {
+        sum += element
+    });
+    callback(sum)
+}
+const numeros = [1, 2, 3, 4, 5];
+sumarArreglo(numeros, function (resultado) {
+    console.log(resultado);
+});
+
+//EJERCICIO #2
+
+function filtrarArreglo(arreglo, callbackFiltro) {
+    return arreglo.filter(callbackFiltro);
+}
+  const numerosPares = filtrarArreglo(numeros, function (numero) {
+    return numero % 2 === 0;
+  });
+  console.log(numerosPares); // Debería imprimir [2, 4]
+
+//EJERCICIO #3
+function filtrarPares(numeros, callback) {
+    const pares = numeros.filter(numero => numero % 2 === 0);
+    callback(pares);
+}
+  
+function mostrarPares(numerosPares) {
+    console.log(numerosPares)
+}
+
+const numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  
+filtrarPares(numeros2, mostrarPares);
+
+//EJERCICIO #4
+const bogota = () => console.log('Bogota');
+
+const madrid = (callback) => {
+    setTimeout(() => {
+        console.log("Madrid");
+        callback();
+    }, 3000);
+};
+
+const alemania = (callback) => {
+    setTimeout(() => {
+        console.log('Alemania');
+        callback();
+    }, 4000);
+};
+
+const corea = (callback) => {
+    setTimeout(() => {
+        console.log('Corea');
+        callback();
+    }, 8000);
+};
+
+bogota();
+madrid(() =>{
+    alemania (() =>{
+        corea(() =>{
+        });
+    });
+});
