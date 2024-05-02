f321b9717a1b7494a26073c0bf817a9b9b6066e6
Rramirezch <rodrigora05@yahoo.com>
2024-05-02 08:33:26 -0500

    entrego Tarea6

---
+++ RODRIGO_RAMIREZ_19016648/Tarea6/punto2.js
@@ -0,0 +1,17 @@
+const estudiantes = [
+    { nombre: "Camilo", edad: 33, promedio: 10 },
+    { nombre: "Juan", edad: 30, promedio: 6 },
+    { nombre: "Andres", edad: 29, promedio: 8.7 },
+];
+
+//Imprime en la consola el nombre del segundo estudiante.
+
+for (let i=0; i<estudiantes.length; i++) {
+    console.log(estudiantes[1].nombre);
+    break
+}
+
+//Actualiza la edad del primer estudiante a 25 años. Imprime en la consola la 
+// información completa del primer estudiante después de laactualización.
+estudiantes[0].edad= 25;
+console.log(estudiantes[0]);
---
+++ RODRIGO_RAMIREZ_19016648/Tarea6/punto4.js
@@ -0,0 +1,73 @@
+const estudiantes = [
+    {
+      nombre: "Ana",
+      edad: 20,
+      promedio: 85,
+      genero: "Femenino",
+      carrera: "Ingeniería Civil",
+      ciudad: "Bogotá",
+    },
+    {
+      nombre: "Juan",
+      edad: 22,
+      promedio: 78,
+      genero: "Masculino",
+      carrera: "Medicina",
+      ciudad: "Medellín",
+    },
+    {
+      nombre: "María",
+      edad: 21,
+      promedio: 90,
+      genero: "Femenino",
+      carrera: "Administración de Empresas",
+      ciudad: "Cali",
+    },
+    {
+      nombre: "Pedro",
+      edad: 23,
+      promedio: 82,
+      genero: "Masculino",
+      carrera: "Derecho",
+      ciudad: "Barranquilla",
+    },
+    {
+      nombre: "Laura",
+      edad: 24,
+      promedio: 88,
+      genero: "Femenino",
+      carrera: "Arquitectura",
+      ciudad: "Cartagena",
+    },
+    {
+      nombre: "Carlos",
+      edad: 20,
+      promedio: 75,
+      genero: "Masculino",
+      carrera: "Ingeniería de Sistemas",
+      ciudad: "Santa Marta",
+    },
+    {
+      nombre: "Sofía",
+      edad: 22,
+      promedio: 95,
+      genero: "Femenino",
+      carrera: "Psicología",
+      ciudad: "Pereira",
+    },
+  ];
+  //Imprime en la consola el nombre y la edad del estudiante con el promedio más alto.
+//const promedioMasAlto = 0;
+let varMax=0;  
+let varIndex=0;      
+estudiantes.forEach((element,index)=>{ 
+
+ if(element.promedio>=varMax){
+    varMax=element.promedio;
+     varIndex=index;
+    
+ }
+});
+console.log('El promedio mas alto es:', varMax);
+console.log('El estudiante con el promedio mas alto es :',
+ estudiantes[varIndex].nombre,'con',estudiantes[varIndex].edad, 'años');
